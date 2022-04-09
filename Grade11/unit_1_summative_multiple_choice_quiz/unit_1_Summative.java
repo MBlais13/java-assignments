f5837097fb6a85e4b2b4ffa6c66017c6776c4a6c
Michael Blais <michaelblais13@gmail.com>
2022-04-08 22:47:08 -0400

    fixed unit1 quiz

--- Grade11/unit_1_summative_multiple_choice_quiz/unit_1_Summative.java
+++ Grade11/unit_1_summative_multiple_choice_quiz/unit_1_Summative.java
@@ -1,139 +1,109 @@
 // Grade 11, Computer Science, Assignment Unit 1
 
-// review needed!
 package unit_1_summative_multiple_choice_quiz;
 
 import java.util.Scanner;
-import java.text.NumberFormat;
+//import java.text.NumberFormat;
 
 class unit_1_Summative {
+    public static Scanner input = new Scanner(System.in);
+    public static int rollingAnswer = 0; // when the user selects an answer it will keep it temporarily
+    public static int rollingWrong = 0; // keeps a total of the wrong answers
+    public static int rollingRight = 0; // keeps a total of the right answers
+    public static boolean passedTest = false; // UNUSED - determines whether you passed the test or not (pass rate is set to 60%)
+    public static int passRate = 60; // the rate it will pass the user. testfinish() function uses this
+
     public static void main(String[] args) {
+        System.out.println("Welcome to my quiz!");
 
-        String guess1;
-        String answer1;
-        String answer2;
-        String answer3;
-        String answer4;
-        String answer5;
-        double wronganswer;
-        double rightanswer;
-        int right1;
-        int right2;
-        int right3;
-        int right4;
-        int right5;
-        int wrong1;
-        int wrong2;
-        int wrong3;
-        int wrong4;
-        int wrong5;
-        double rightanswerDivided;
+        questions(); // prompts user with questions
+        testfinish(); // displays final test scores
+    }
 
-        System.out.println("Welcome to the cool guy quiz");
-
-        Scanner input = new Scanner(System.in); // starts input process
-
+    static void questions() {
         // Question 1
-        System.out.println("How many seasons are there of the simpsons?");
-        System.out.println("a. 20");
-        System.out.println("b. 29");
-        System.out.println("c. 33");
-        System.out.println("d. 40");
-        System.out.println("please choose one answer");
-        answer1 = input.next();
-
-        if (answer1.equals("c")) {
+        System.out.println(rollingRight);
+        System.out.println(
+                "--------------------------\nHow many terabytes of free space do I have at the time of writing this?");
+        System.out.println("1 : 1\n2 : 8\n3 : 24\n4 : not enough");
+        rollingAnswer = input.nextInt();
+        if (rollingAnswer == 4) {
+            rollingRight = rollingRight + 1;
             System.out.println("Correct!");
-            right1 = 1;
-            wrong1 = 0;
         } else {
-            right1 = 0;
-            wrong1 = 1;
-            System.out.println("Incorrect!");
+            rollingWrong = rollingWrong + 1;
+            System.out.println("Sorry, wrong answer.");
         }
 
         // Question 2
-        System.out.println("How many fingers do you have on your hand?");
-        System.out.println("a. 20");
-        System.out.println("b. 5");
-        System.out.println("c. 3");
-        System.out.println("d. 4");
-        System.out.println("please choose one answer");
-        answer2 = input.next();
-
-        if (answer2.equals("d")) {
+        System.out.println("--------------------------\nDo I like school?");
+        System.out.println(
+                "1 : Yes\n2 : I find it fun\n3 : Absolutely not!\n4 : I wish I could spend more time at school");
+        rollingAnswer = input.nextInt();
+        if (rollingAnswer == 3) {
+            rollingRight = rollingRight + 1;
             System.out.println("Correct!");
-            right2 = 1;
-            wrong2 = 0;
         } else {
-            right2 = 0;
-            wrong2 = 1;
-            System.out.println("Incorrect");
+            rollingWrong = rollingWrong + 1;
+            System.out.println("Sorry, wrong answer.");
         }
+
         // Question 3
-        System.out.println("How many stars are there in our solar system?");
-        System.out.println("a. 1");
-        System.out.println("b. 2");
-        System.out.println("c. 1.5");
-        System.out.println("d. 1.1");
-        answer3 = input.next();
-
-        if (answer3.equals("a")) {
+        System.out.println("--------------------------\nWhat am I interested in?");
+        System.out.println("1 : Computers\n2 : I.T\n3 : Game development\n4 : All of the above");
+        rollingAnswer = input.nextInt();
+        if (rollingAnswer == 4) {
+            rollingRight = rollingRight + 1;
             System.out.println("Correct!");
-            right3 = 1;
-            wrong3 = 0;
         } else {
-            right3 = 0;
-            wrong3 = 1;
-            System.out.println("Incorrect");
+            rollingWrong = rollingWrong + 1;
+            System.out.println("Sorry, wrong answer.");
         }
+
         // Question 4
-        System.out.println("How many doors to your house do you have with locks on them?");
-        System.out.println("a. 1");
-        System.out.println("b. 2");
-        System.out.println("c. 4");
-        System.out.println("d. 5");
-        answer4 = input.next();
-
-        if (answer4.equals("a")) {
+        System.out.println("--------------------------\nWhy do we have to use java?");
+        System.out.println(
+                "1 : Because Java sucks\n2 : All of the above\n3 : Java is amazing\n4 : Java is the best language");
+        rollingAnswer = input.nextInt();
+        if (rollingAnswer == 1) {
+            rollingRight = rollingRight + 1;
             System.out.println("Correct!");
-            right4 = 1;
-            wrong4 = 0;
         } else {
-            right4 = 0;
-            wrong4 = 1;
-            System.out.println("Incorrect you now have 1!");
+            rollingWrong = rollingWrong + 1;
+            System.out.println("Sorry, wrong answer.");
         }
+
         // Question 5
-        System.out.println("What is my name?");
-        System.out.println("a. Carl");
-        System.out.println("b. Sam");
-        System.out.println("c. Han");
-        System.out.println("d. David");
-        answer5 = input.next();
-
-        if (answer5.equals("d")) {
-            right5 = 1;
-            wrong5 = 0;
+        System.out.println("--------------------------\nWhats my last name?");
+        System.out.println("1 : Blais\n2 : Blaise\n3 : Blaze\n4 : Reaume");
+        if (rollingAnswer == 1) {
+            rollingRight = rollingRight + 1;
+            System.out.println("Correct!");
+        } else if (rollingAnswer == 4) {
+            rollingRight = rollingRight + 1;
             System.out.println("Correct!");
         } else {
-            right5 = 0;
-            wrong5 = 1;
-            System.out.println("Incorrect!");
+            rollingWrong = rollingWrong + 1;
+            System.out.println("Sorry, wrong answer.");
         }
-        NumberFormat percent = NumberFormat.getPercentInstance();// starts percent program
+    }
 
-        rightanswer = right1 + right2 + right3 + right4 + right5;// does math to find out how much you got right out of
-                                                                 // five
-        wronganswer = wrong1 + wrong2 + wrong3 + wrong4 + wrong5;// does math to find out how much you got wrong out of
-                                                                 // five
-        rightanswerDivided = rightanswer / 5;// does math to get you percent avererage
+    static void testfinish() {
+        double totalQuestions = 5;
+        double totalpercent = rollingRight / totalQuestions * 100;
 
-        System.out.println("On this quiz you got " + rightanswer + " out of 5.0 right on this quiz");
-        System.out.println("On this quiz you got " + wronganswer + " out of 5.0 wrong on this quiz");
-        //System.out.println("This is your score on the quiz:");  // tells you your percent average
-        //System.out.printf("This is your score on the quiz:\n",percent.format(rightanswerDivided));
-        System.out.printf(percent.format(rightanswerDivided));
+        if (totalpercent >= passRate) { // passed quiz (pass rate is set to 60%)
+            passedTest = true; 
+            System.out.println("--------------------------\n- You Passed! -\n--------------------------");
+            System.out.println(totalpercent + "%!");
+            System.out.println("You got " + rollingRight + " questions correct!");
+            System.out.println("You got " + rollingWrong + " questions incorrect.");
+        } else { // did not pass quiz
+            System.out.println("--------------------------\n- You did not pass :( -\n--------------------------");
+            System.out.println(totalpercent + "%");
+            System.out.println("You got " + rollingRight + " questions correct.");
+            System.out.println("You got " + rollingWrong + " questions incorrect.");
+        }
     }
 }
 
